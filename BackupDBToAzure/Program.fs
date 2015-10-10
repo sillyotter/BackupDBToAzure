8fc1ab77f98896d3c28eec5c787856f85dbb9124
Guy Oliver <guy.oliver@gmail.com>
2015-10-10 04:46:28 -0400

    config cleanup

--- BackupDBToAzure/Program.fs
+++ BackupDBToAzure/Program.fs
@@ -9,8 +9,8 @@ open System.Text.RegularExpressions
 
 type Settings = AppSettings< "App.config" >
 
-let possiblyCreateCredentials constr credName key account = 
-    async { 
+let possiblyCreateCredentials constr credName key account =
+    async {
         use con = new SqlConnection(constr)
         do! con.OpenAsync() |> Async.AwaitTask
         use cmd = con.CreateCommand()
@@ -25,8 +25,8 @@ let possiblyCreateCredentials constr credName key account =
         con.Close()
     }
 
-let backupDatabase container (csb : SqlConnectionStringBuilder) credName = 
-    async { 
+let backupDatabase container (csb : SqlConnectionStringBuilder) credName =
+    async {
         let targetUrl = Uri(Settings.TargetUrl, (sprintf "%s/%s-%s.bak" container csb.InitialCatalog (DateTimeOffset.UtcNow.ToString("yyyyMMddTHHmmss"))))
         use con = new SqlConnection(csb.ConnectionString)
         do! con.OpenAsync() |> Async.AwaitTask
@@ -34,8 +34,8 @@ let backupDatabase container (csb : SqlConnectionStringBuilder) credName =
         cmd.CommandType <- CommandType.Text
         cmd.CommandTimeout <- int (TimeSpan.FromHours(6.0).TotalSeconds)
         cmd.CommandText <- @"
-            BACKUP DATABASE " + csb.InitialCatalog + @" 
-            TO URL = '" + (targetUrl.ToString()) + @"' 
+            BACKUP DATABASE " + csb.InitialCatalog + @"
+            TO URL = '" + (targetUrl.ToString()) + @"'
             WITH CREDENTIAL = '" + credName + "'"
         do! cmd.ExecuteNonQueryAsync()
             |> Async.AwaitTask
@@ -43,22 +43,22 @@ let backupDatabase container (csb : SqlConnectionStringBuilder) credName =
         con.Close()
     }
 
-let getTargetConnectionStrings prefix = 
+let getTargetConnectionStrings prefix =
     let csettings = ConfigurationManager.ConnectionStrings
     let cstrings = Array.create<ConfigurationElement> csettings.Count null
     csettings.CopyTo(cstrings, 0)
     cstrings
-    |> Array.choose (function 
+    |> Array.choose (function
            | :? ConnectionStringSettings as cs -> Some(cs)
            | _ -> None)
-    |> Array.choose (fun x -> 
+    |> Array.choose (fun x ->
            if x.Name.StartsWith(prefix) then Some(x.ConnectionString)
            else None)
     |> Array.map (fun x -> SqlConnectionStringBuilder(x))
 
 [<EntryPoint>]
-let main _ = 
-    if Environment.UserInteractive then 
+let main _ =
+    if Environment.UserInteractive then
         let cct = new NLog.Targets.ColoredConsoleTarget(Layout = NLog.Layouts.Layout.FromString @"${date:format=yyyy-MM-ddTHH\:mm\:ss.fff}|${level}|${logger}|${message}|${exception:innerFormat=ShortType,Method,Message:maxInnerExceptionLevel=3:innerExceptionSeparator=^:separator=#:format=ShortType,Method,Message}")
         let rule1 = new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, cct)
         NLog.LogManager.Configuration.AddTarget("console", cct)
@@ -66,8 +66,8 @@ let main _ =
         NLog.LogManager.ReconfigExistingLoggers()
 
     let logger = NLog.FSharp.Logger()
-    
-    try 
+
+    try
         let azureConStr = Settings.ConnectionStrings.AzureStorage
         let matchRegexString = Settings.MatchRegex
         let matchRegex = new Regex(matchRegexString, RegexOptions.Compiled)
@@ -75,20 +75,20 @@ let main _ =
         let containerName = Settings.Container
         let credName = Settings.CredentialsName
         let targets = getTargetConnectionStrings "Target"
-        
-        let mastersOfEachServer = 
+
+        let mastersOfEachServer =
             targets
             |> Array.groupBy (fun x -> x.DataSource)
             |> Array.map (fun (_, cs) -> cs.[0])
-            |> Array.map (fun c -> 
+            |> Array.map (fun c ->
                    let ncs = SqlConnectionStringBuilder(c.ConnectionString)
                    ncs.InitialCatalog <- "master"
                    ncs)
-        
+
         let storageAccount = CloudStorageAccount.Parse(azureConStr)
 
         mastersOfEachServer
-        |> Array.map (fun x -> 
+        |> Array.map (fun x ->
                logger.Info "Creating credentials on server %s if needed." x.DataSource
                possiblyCreateCredentials x.ConnectionString credName (storageAccount.Credentials.ExportBase64EncodedKey()) storageAccount.Credentials.AccountName)
         |> Async.Parallel
@@ -96,7 +96,7 @@ let main _ =
         |> ignore
 
         targets
-        |> Array.map (fun x -> 
+        |> Array.map (fun x ->
                logger.Info "Backing up %s." x.InitialCatalog
                backupDatabase containerName x credName)
         |> Async.Parallel
@@ -107,12 +107,12 @@ let main _ =
         let container = blobClient.GetContainerReference(containerName)
 
         container.ListBlobs(useFlatBlobListing = true)
-        |> Seq.choose (function 
+        |> Seq.choose (function
                | :? CloudPageBlob as pb -> Some(pb)
                | _ -> None)
         |> Seq.filter (fun x -> matchRegex.IsMatch(x.Name) && (x.Properties.LastModified.Value < (DateTimeOffset.Now - age)))
-        |> Seq.map (fun x -> 
-               async { 
+        |> Seq.map (fun x ->
+               async {
                    logger.Info "Deleting old backup %s created at %s" x.Name (x.Properties.LastModified.Value.ToString())
                    do! x.DeleteAsync() |> Async.AwaitTask
                })
